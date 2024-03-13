56442ee931568ef4ca825806494ab5294273f361
최정필 <wjdvlf1407@gmail.com>
2024-03-13 15:25:40 +0900

    스트라이크 볼 기능 추가

--- src/game.cpp
+++ src/game.cpp
@@ -6,8 +6,28 @@ using namespace std;
 
 int game(int answer, int guess, int chances) {
 
-	int strikes;
-	int balls;
+	int aArray[4];
+	int gArray[4];
+
+	aArray[0] = answer;
+	aArray[1] = answer / 100;
+	answer = answer % 100;
+	aArray[2] = answer / 10;
+	answer = answer % 10;
+	aArray[3] = answer;
+
+	gArray[0] = guess;
+	gArray[1] = guess / 100;
+	guess = guess % 100;
+	gArray[2] = guess / 10;
+	guess = guess % 10;
+	gArray[3] = guess;
+
+	answer = aArray[0];
+	guess = gArray[0];
+
+	int strikes = 0;
+	int balls = 0;
 
 	if (answer == guess) {
 		cout << "You win!" << endl;
@@ -17,13 +37,25 @@ int game(int answer, int guess, int chances) {
 		cout << "You lose!" << endl;
 
 	} else {
+
+		for (int i = 1; i < 4; i++) {
+			for (int j = 1; j < 4; j++) {
+				if (aArray[i] == gArray[j] && i == j) {
+					strikes++;
+
+				} else if (aArray[i] == gArray[j] && i != j) {
+					balls++;
+				}
+
+			}
+		}
+
 		cout << "Strikes: " << strikes;
 		cout << ", Balls: " << balls << endl;
 
 	}
 
 	chances--;
-
 
 	return chances;
 }
