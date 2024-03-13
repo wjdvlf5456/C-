a0a42892e6c79d64f5e21c5a9954e6b1331759e5
최정필 <wjdvlf1407@gmail.com>
2024-03-13 15:49:55 +0900

    util 추가중

--- src/main.cpp
+++ src/main.cpp
@@ -2,18 +2,25 @@
 #include <stdio.h>
 using namespace std;
 
-extern bool game(int answer, int guess,bool flag);
+extern bool game(int answer, int guess, bool flag);
+extern int util();
 
 
 int main() {
 	
-	int answer;
+	int answer = util();
+	
+	
 	int guess;
 	//int chances = 5;
 	bool flag = false;
 	
-	cout << "Enter a answer: " <<"";
-	cin >> answer;
+	//cout << "Enter a answer: " <<"";
+	//cin >> answer;
+	
+	
+	
+	
 	
 	while(flag == false){
 		cout << "Enter a guess: " << "";
--- src/util.cpp
+++ src/util.cpp
@@ -4,16 +4,11 @@ using namespace std;
 
 // 숫자 랜덤 생성
 
-int main() {
-	
+int util() {
+
 	int answer;
-	int guess;
-	
-	cout << "Enter a answer: " <<"";
-	cin >> answer;
-	
-	cout << "Enter a guess: " << "";
-	cin >> guess;
-	
-	return 0;
+
+	answer = rand() % 999;
+
+	return answer;
 }
