4f78160453af7183807e265a07b263efef4ce4be
최정필 <wjdvlf1407@gmail.com>
2024-03-19 10:38:20 +0900

    util 난수 작성

--- src/util.cpp
+++ src/util.cpp
@@ -8,7 +8,36 @@ int util() {
 
 	int answer;
 
-	answer = rand() % 999;
+	int hundred;
+	int ten;
+	int one;
+
+	hundred = rand() % 10;
+
+	bool flag = false;
+	while (flag == false) {
+		ten = rand() % 10;
+		if (ten != hundred) {
+			flag = true;
+
+		}
+
+	}
+
+	flag = false;
+
+	while (flag == false) {
+		one = rand() % 10;
+		if (one != ten && one != hundred) {
+			flag = true;
+
+		}
+
+	}
+
+	answer = (hundred * 100) + (ten * 10) + one;
+
+	cout << answer << endl;
 
 	return answer;
 }
